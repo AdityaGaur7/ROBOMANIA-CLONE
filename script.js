d6de8c9f895679b26aa37e54bce5e45047686cb1
AdityaGaur7 <adgaur027>
2023-05-13 22:08:45 +0530

    clone_by_adgaur

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function f1(){
+    // alert("hello");
+    document.getElementById("cls").style.display="flex";
+    document.getElementById("opn").style.display="none";
+   document.querySelector(".sidebar").style.display="block";
+}
+function f2(){
+    // alert("bye");
+    document.getElementById("opn").style.display="flex";
+
+    document.getElementById("cls").style.display="none";
+    document.querySelector(".sidebar").style.display="none";
+
+}
+
+
